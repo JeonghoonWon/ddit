e6d41492cf70e8c8f7ab89bbec825575bb0d88f7
JeonghoonWon <expedition1205@gmail.com>
2021-03-20 11:48:04 +0900

    no message

--- Python_mysql 정리.sql
+++ Python_mysql 정리.sql
@@ -1,2 +1,28 @@
+# 컬럼명과 시간 데이터 출력.
 SELECT s000020, in_time FROM stock_sync_0121
 ORDER BY in_time
+
+#테이블당 컬럼개수 count
+SELECT count(*)
+FROM information_schema.columns
+WHERE table_name = 'stock_sync_0121'
+
+#in_time 컬럼 제외 count
+SELECT count(*)
+FROM information_schema.columns
+WHERE table_name = 'stock_sync_0121'
+AND COLUMN_NAME != 'in_time'
+
+
+# 컬럼명 추출.
+SELECT  COLUMN_NAME
+FROM    INFORMATION_SCHEMA.COLUMNS
+WHERE   TABLE_NAME = 'stock_sync_0121'
+
+#in_time 컬럼 제외 컬럼명 추출
+SELECT  COLUMN_NAME
+FROM    INFORMATION_SCHEMA.COLUMNS
+WHERE   TABLE_NAME = 'stock_sync_0121'
+AND COLUMN_NAME != 'in_time'
+
+
