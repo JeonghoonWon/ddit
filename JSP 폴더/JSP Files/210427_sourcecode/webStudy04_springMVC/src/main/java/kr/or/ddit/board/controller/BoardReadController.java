ebb27200890e2870db871462fcb95180e50f1aeb
JeonghoonWon <expedition1205@gmail.com>
2021-04-28 19:59:40 +0900

    JSP_파일

--- JSP 폴더/JSP Files/210427_sourcecode/webStudy04_springMVC/src/main/java/kr/or/ddit/board/controller/BoardReadController.java
+++ JSP 폴더/JSP Files/210427_sourcecode/webStudy04_springMVC/src/main/java/kr/or/ddit/board/controller/BoardReadController.java
@@ -35,13 +35,14 @@ import kr.or.ddit.vo.PagingVO;
 @Controller
 public class BoardReadController {
 	public static final String BOARDAUTH = "board.authenticated";
+	// injection 을 사용하려면 처음부터 끝까지 Inject 가 되어야 한다.
 	@Inject
 	private IBoardService service;
 	@Inject
-	private WebApplicationContext container;
+	private WebApplicationContext container;  //web 에서 사용하기 때문에 WebApplicationContext / 주입을 위해 inject annotation 사용해준다. 하위 컨테이너의 레퍼런스가 들어가있음.
 	private ServletContext application;
 	@PostConstruct
-	public void init() {
+	public void init() {  //@Inject이 된 이후에 발생하게된다!
 		application = container.getServletContext();
 	}
 	
@@ -50,13 +51,15 @@ public class BoardReadController {
 		@RequestParam("bo_no") int bo_no
 		, @RequestParam("bo_pass") String bo_pass
 		, HttpSession session
+			// 처음부터 BoardVO search 를 사용하게되면 인증처리가 어렵기 때문에 따로따로 받아서 검증 처리 함.but, 꼭 이게 답은 아니다.
 	) {
 		BoardVO search = new BoardVO();
 		search.setBo_no(bo_no);
 		search.setBo_pass(bo_pass);
 		String view = null;
-		if(service.boardAuthenticate(search)) {
+		if(service.boardAuthenticate(search)) { // session scope 에 BOARDAUTH 가 있는지없는지에따라 확인가능
 			session.setAttribute(BOARDAUTH, search);
+				// 더이상 bo_no, bo_pass를 더이상 사용할 필요가 없다. 그리고 인증은 redirect 사용한다.
 			view = "redirect:/board/boardView.do?what="+bo_no;
 		}else {
 			session.setAttribute("message", "비밀번호 오류");
@@ -65,7 +68,7 @@ public class BoardReadController {
 		return view;
 	}
 	
-	@RequestMapping("/board/boardView.do")
+	@RequestMapping("/board/boardView.do") // 일반은 html 로 보내는 것. // 하나의 게시글 상세정보
 	public String view(
 		@RequestParam("what") int bo_no	
 		, @ModelAttribute("search") BoardVO search
@@ -77,6 +80,8 @@ public class BoardReadController {
 		
 		boolean valid = true;
 		if("Y".equals(board.getBo_sec())) {
+			// 여기까지 오는 과정에서 인증을 거쳤는지 확인
+			// 인증을 거쳤다면 그 인증 결과가 어디에 저장되어있는지 확인.
 			BoardVO authenticated = 
 					(BoardVO) session.getAttribute(BOARDAUTH);
 			if(authenticated==null || authenticated.getBo_no() != bo_no) {
@@ -89,15 +94,16 @@ public class BoardReadController {
 			model.addAttribute("board", board);
 			view = "board/boardView";
 		}else {
+			//비밀글인데 인증을 거치지 않은 경우
 			view = "board/passwordForm";
-		}
+		}// if(valid) end
 		return view;
 	}
 	
 	@RequestMapping(value="/board/boardView.do"
 			, produces = "text/plain;charset=UTF-8")
 	@ResponseBody
-	public String preview(
+	public String preview( // 비동기로 왔을때 json 으로 보내는것.
 			@RequestParam("what") int bo_no
 			, @ModelAttribute("search") BoardVO search
 			, HttpSession session
@@ -106,25 +112,31 @@ public class BoardReadController {
 		BoardVO board = service.retrieveBoard(search);
 		
 		boolean valid = true;
-		if("Y".equals(board.getBo_sec())) {
+		if("Y".equals(board.getBo_sec())) {// 비정상적인 상황
+			// 여기까지 오는 과정에서 인증을 거쳤는지 확인
+			// 인증을 거쳤다면 그 인증 결과가 어디에 저장되어있는지 확인.
 			BoardVO authenticated = 
 					(BoardVO) session.getAttribute(BOARDAUTH);
 			if(authenticated==null || authenticated.getBo_no() != bo_no) {
 				valid = false;
 			}
+			// 동기요청에대한 헨들러로 	
 		}
 		
-		if(valid) {
+		if(valid) { // 정상적인 상황
+			// session에 남아있는 인증을 지워줘야한다.
 			session.removeAttribute(BOARDAUTH);
+	// 직렬화를 위한 내용이기떄문에 논리적인 뷰 네임화 차별을 주기위해 responcebody annotation
 			return board.getBo_content();
 		}else {
+				// 비정상적인 상황
 			throw new BadRequestException("비밀글은 미리보기 불가.");
 		}
 	}
 	
 	@RequestMapping(value="/board/noticeList.do"
 			, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseBody
+	@ResponseBody // 비동기로  json 처리하는경우 적용.
 	public PagingVO<BoardVO> noticeListForAjax(
 			@RequestParam(value="searchType", required=false) String searchType
 			, @RequestParam(value="searchWord", required=false) String searchWord
@@ -138,7 +150,7 @@ public class BoardReadController {
 		return listForAjax(currentPage, searchType, 
 				searchWord, minDate, maxDate);
 	}
-	@RequestMapping("/board/noticeList.do")
+	@RequestMapping("/board/noticeList.do") // 일반적인 요청처리
 	public String noticeList(
 			@RequestParam(value="searchType", required=false) String searchType
 			, @RequestParam(value="searchWord", required=false) String searchWord
@@ -153,7 +165,7 @@ public class BoardReadController {
 				searchWord, minDate, maxDate, model);
 	}
 	
-	@RequestMapping(value={"/board/boardList.do"})
+	@RequestMapping(value={"/board/boardList.do"})  //value를 배열로 넣는다. value[]
 	public String listForHTML(
 			@RequestParam(value="page", required=false, defaultValue="1") int currentPage
 			, @RequestParam(value="searchType", required=false) String searchType
@@ -171,6 +183,8 @@ public class BoardReadController {
 		
 	}
 	
+	// 동기와 비동기에 대한 처리가 필요하다.
+	// 클라이언트가 html/ json중 어떤걸 요청하는지 파악. accept header 를 통해 확인.
 	@RequestMapping(value="/board/boardList.do", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public PagingVO<BoardVO> listForAjax(
@@ -203,11 +217,13 @@ public class BoardReadController {
 			if(source == null) {
 				continue;
 			}
-			Document dom = Jsoup.parse(source);
+			Document dom = Jsoup.parse(source);   // dom 트리 구조 생성
 			Elements imgs = dom.getElementsByTag("img");
 			String thumbnail = application.getContextPath() + "/images/cat1.jpg";
 			if(!imgs.isEmpty()) {
-				Element img = imgs.get(0);
+			// 썸네일로 보여줄 사진이 없는 경우 하나를 생성해줘야함.
+	    		  // src 에 들어가있는 주소값을 뽑아와야한다.
+	    		 Element img = imgs.get(0); // 첫번째 사진을 썸네일로
 				thumbnail = img.attr("src");
 			}
 			tmp.setThumbnail(thumbnail);
